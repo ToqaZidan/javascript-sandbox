ef5547a860dc96d8f9503e8a867837c8f3a40691
ToqaZidan <toqazidan@hotmail.com>
2024-02-17 22:39:55 +0200

    some changes

--- 01-variables-data-types/08-type-coercion/script.js
+++ 01-variables-data-types/08-type-coercion/script.js
@@ -1,28 +1,28 @@
 let x;
 
 // Coerced to a string
-x = 5 + '5';
+x = 5 + '5'; // '55'
 
-x = 5 + Number('5');
+x = 5 + Number('5'); // 10
 
 // Coerced to a number
-x = 5 * '5';
+x = 5 * '5'; // 25 (coerced to a number)
 
 // null is coerced to 0 as it is a `falsy` value
-x = 5 + null;
+x = 5 + null; // 5 
 
-x = Number(null);
+x = Number(null);  // 0
 
-x = Number(true);
-x = Number(false);
+x = Number(true); // 1
+x = Number(false); // 0
 
 // true is coerced to a 1
-x = 5 + true;
+x = 5 + true; // 6
 
 // false is coerced to 0 (falsy)
-x = 5 + false;
+x = 5 + false; // 5
 
 // Undefined is coerced to 0 (falsy)
-x = 5 + undefined;
+x = 5 + undefined; // NaN
 
 console.log(x, typeof x);
